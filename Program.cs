d1fe040cd91e80ce18f954d37151cd604010ab44
Thor271998 <thor.petersen@specialisterne.dk>
2025-10-09 05:45:38 -0700

    Cleaning up some code

--- Program.cs
+++ Program.cs
@@ -13,16 +13,19 @@ using ExcelDataReader;
 using System.Data;
 using OfficeOpenXml;
 using ClosedXML.Excel;
+using PDFDownload;
 
 namespace PDFDownloader
 {
     class Program
     {
-        //private static readonly HttpClient _httpClient = new HttpClient();
-
         static async Task Main(string[] args)
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            //Creating instances of the Downloader and Reader classes
+            Downloader downloader = new Downloader();
+            Reader reader = new Reader();
 
             //Path for the list of URLs
             string listPath = @"C:\Visual Studio Projecter\PDFDownload\PDFDownload\List Folder\GRI_2017_2020 (1).xlsx";
@@ -36,194 +39,29 @@ namespace PDFDownloader
             //Path for existing downloads
             string dwnPath = @"C:\Visual Studio Projecter\PDFDownload\PDFDownload\Output\dwn\";
 
-            //using(var workbook = new XLWorkbook(listPath))
-            //{
-            //    var worksheet = workbook.Worksheet(1);
-            //    var dataTable = new DataTable();
+            //Getting a list of existing PDF files in the download directory
+            string[] existingFiles = Directory.GetFiles(dwnPath, "*.pdf");
 
-            //    foreach (var cell in worksheet.Row(1).Cells())
-            //    {
-            //        dataTable.Columns.Add(cell.Value.ToString());
-            //    }
+            //Reading the Excel file using the Reader class
+            DataTable dataTable = new DataTable();
+            dataTable = reader.ReadFile(listPath);
 
-            //    foreach (var row in worksheet.RowsUsed().Skip(1)) // Skip header row
-            //    {
-            //        var dataRow = dataTable.NewRow();
-            //        for (int i = 0; i < dataTable.Columns.Count; i++)
-            //        {
-            //            dataRow[i] = row.Cell(i + 1).Value.ToString();
-            //        }
-            //        dataTable.Rows.Add(dataRow);
-            //    }
-
-            //    foreach (DataRow row in dataTable.Rows)
-            //    {
-            //        if (row[URL].ToString() == "")
-            //            continue;
-            //        string? id = row[ID]?.ToString();
-            //        string? url = row[URL]?.ToString();
-            //        Console.WriteLine($"ID: {id}, URL: {url}");
-            //    }
-            //}
-
-            using (FileStream stream = File.Open(listPath, FileMode.Open, FileAccess.Read))
+            //Iterating through each row in the DataTable
+            foreach (DataRow row in dataTable.Rows) 
             {
-                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
-                DataTable dataTable = excelReader.AsDataSet().Tables[0];
-                excelReader.Close();
+                //Extracting the ID and URLs from the row
+                string? id = row[0].ToString();
+                string pdfName = id + ".pdf";
+                string? url = row[37]?.ToString();
+                string? secondaryUrl = row[38]?.ToString();
 
-                foreach (DataRow row in dataTable.Rows)
-                {
-                    if(row[37].ToString() == "")
-                        continue;
+                //Skipping the download if the URL is empty or if the file already exists
+                if (/*url == "" || */existingFiles.Contains(pdfName))
+                    continue;
 
-                    string ?id = row[0]?.ToString();
-                    string pdfName = id + ".pdf";
-                    string ?url = row[37]?.ToString();
-                    string ?secondaryUrl = row[38]?.ToString();
-                    bool secondaryUrlProper = secondaryUrl != "" && secondaryUrl != null;
-                    //Console.WriteLine($"ID: {id}, URL: {url}");
-
-                    if (id.Equals("BR50071"))
-                    {
-                        Console.WriteLine("Debug stop");
-                    }
-
-                    using (HttpClient client = new HttpClient())
-                    {
-                        try
-                        {
-                            HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
-                            response.EnsureSuccessStatusCode();
-
-                            if (!response.IsSuccessStatusCode)
-                            {
-                                throw new Exception($"Failed to download PDF. Status code: {response.StatusCode}");
-                            }
-
-                            if (response.Content.Headers.ContentType?.MediaType != "application/pdf")
-                            {
-                                throw new Exception("The URL does not point to a valid PDF file.");
-                            }
-
-                            byte[] pdfBytes = await response.Content.ReadAsByteArrayAsync();
-
-                            string filePath = Path.Combine(dwnPath, pdfName);
-
-                            await File.WriteAllBytesAsync(filePath, pdfBytes);
-
-                            using (StreamWriter writer = File.AppendText(statusPath))
-                            {
-                                writer.WriteLine(pdfName + ": Successfully downloaded.");
-                                writer.Close();
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            try
-                            {
-                                HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
-                                response.EnsureSuccessStatusCode();
-
-                                if (!response.IsSuccessStatusCode)
-                                {
-                                    throw new Exception($"Failed to download PDF. Status code: {response.StatusCode}");
-                                }
-
-                                if (response.Content.Headers.ContentType?.MediaType != "application/pdf")
-                                {
-                                    throw new Exception("The URL does not point to a valid PDF file.");
-                                }
-
-                                byte[] pdfBytes = await response.Content.ReadAsByteArrayAsync();
-
-                                string filePath = Path.Combine(dwnPath, pdfName);
-
-                                await File.WriteAllBytesAsync(filePath, pdfBytes);
-
-                                using (StreamWriter writer = File.AppendText(statusPath))
-                                {
-                                    writer.WriteLine(pdfName + ": Successfully downloaded.");
-                                    writer.Close();
-                                }
-                            }
-                            catch (Exception innerEx)
-                            {
-                                Console.WriteLine($"An error occurred: {innerEx.Message}");
-                                using (StreamWriter writer = File.AppendText(statusPath))
-                                {
-                                    writer.WriteLine(pdfName + ": Failed to download.");
-                                    writer.Close();
-                                }
-                            }
-
-                            //Console.WriteLine($"An error occurred: {ex.Message}");
-                            //using (StreamWriter writer = File.AppendText(statusPath))
-                            //{
-                            //    writer.WriteLine(pdfName + ": Failed to download.");
-                            //    writer.Close();
-                            //}
-                        }
-                    }
-                }
+                //Downloading the PDF file using the Downloader class if it isn't already downloaded
+                downloader.DownloadFile(url, dwnPath, statusPath, pdfName, secondaryUrl);
             }
-
-            //using (_httpClient)
-            //{
-            //    try
-            //    {
-            //        HttpResponseMessage response = await _httpClient.GetAsync("http://cdn12.a1.net/m/resources/media/pdf/A1-Umwelterkl-rung-2016-2017.pdf", HttpCompletionOption.ResponseHeadersRead);
-            //        response.EnsureSuccessStatusCode();
-
-            //        if (!response.IsSuccessStatusCode)
-            //        {
-            //            throw new Exception($"Failed to download PDF. Status code: {response.StatusCode}");
-            //        }
-
-            //        if (response.Content.Headers.ContentType?.MediaType != "application/pdf")
-            //        {
-            //            throw new Exception("The URL does not point to a valid PDF file.");
-            //        }
-
-            //        byte[] pdfBytes = await response.Content.ReadAsByteArrayAsync();
-
-            //        string filePath = Path.Combine(dwnPath, "test.pdf");
-
-            //        await File.WriteAllBytesAsync(filePath, pdfBytes);
-
-            //        using (StreamWriter writer = File.AppendText(statusPath))
-            //        {
-            //            writer.WriteLine("test.pdf: Successfully downloaded.");
-            //            writer.Close();
-            //        }
-
-            //        Console.WriteLine("PDF downloaded successfully.");
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine($"An error occurred: {ex.Message}");
-            //        using (StreamWriter writer = File.AppendText(statusPath))
-            //        {
-            //            writer.WriteLine("test.pdf: Failed to download.");
-            //            writer.Close();
-            //        }
-            //        Console.WriteLine("PDF failed to download.");
-            //    }
-            //}
-        }
-    }
-
-    public class ExcelRow
-    {
-        public string ID { get; set; }
-        public string Url { get; set; }
-        public string ATStatus { get; set; }
-
-        public ExcelRow(string id, string url, string atStatus)
-        {
-            ID = id;
-            Url = url;
-            ATStatus = atStatus;
         }
     }
 }
---
+++ Reader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using Newtonsoft.Json;
+using ExcelDataReader;
+using System.Data;
+using OfficeOpenXml;
+using ClosedXML.Excel;
+
+namespace PDFDownload
+{
+    //Defining a class for reading Excel files
+    public class Reader
+    {
+        //Defining the method that will read an Excel file and return a DataTable containing the data
+        public DataTable ReadFile(string filePath)
+        {
+            //Defining a DataTable to hold the data
+            DataTable dataTable = new DataTable();
+
+            //Reading the excel file using FileStream and ExcelDataReader
+            //The 'using' statement ensures that the FileStream is properly disposed of after use
+            //The dataTable is used to store the data read from the Excel file 
+            using (FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                dataTable = excelReader.AsDataSet().Tables[0];
+                excelReader.Close();
+            }
+            
+            return dataTable;
+        }
+    }
+}
